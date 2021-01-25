ce1b65b8df597a10ff72dfd0c03069cf1bf4afb3
Christine Ross <christinejoyross@gmail.com>
2021-01-25 14:44:35 -0800

    init

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    MONGO_URI : 'mongodb+srv://ChristineAdmin:kABU1iK10UXPZZp1@cluster0.x2pt4.mongodb.net/MicroBlogger?retryWrites=true&w=majority',
+    SECRET_KEY: 'pizza'
+}
+
+
